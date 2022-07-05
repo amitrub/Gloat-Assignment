65c9efd2775dde54fe6a5beea0dfb05deb52d895
Amit <amitrub@post.bgu.ac.il>
2022-07-05 13:17:01 +0300

    add base models

---
+++ matcher/jobs/models.py
@@ -0,0 +1,15 @@
+import json
+
+from django.db import models
+from skills.models import Skill
+
+
+class Job(models.Model):
+    """Job Model"""
+
+    title = models.CharField(max_length=255)
+    skills = models.ManyToManyField(Skill)
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                          sort_keys=True, indent=4)
