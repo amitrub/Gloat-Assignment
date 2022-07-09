ce97a56a57c13d34b23c4e0f7045cc3b5fba1eed
Amit <amitrub@post.bgu.ac.il>
2022-07-09 17:41:42 +0300

    fix setup file

--- matcher/setup.sh
+++ matcher/setup.sh
@@ -2,5 +2,5 @@
 
 pip install -r requirements.txt
 python manage.py migrate
-python manage.py loaddata /app/fixtures/job_matcher.json --app job_matcher &&
+python manage.py loaddata fixtures/job_matcher.json --app job_matcher
 python manage.py runserver
